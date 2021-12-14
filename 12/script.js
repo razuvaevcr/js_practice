3f766929e7348a064c3550b23b6d366460ef75db
razuvaevcr <razuvaj@gmail.com>
2021-12-14 16:17:18 +0300

    lesson 15 add cycles

--- 12/script.js
+++ 12/script.js
@@ -10,10 +10,59 @@ const personalMovieDB = {
     privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов?', '23'),
-    b = prompt('Оцените его по пятибальной шкале!', '5'),
-    c = prompt('Один из последних просмотренных фильмов?', '23'),
-    d = prompt('Оцените его по пятибальной шкале!', '5');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+
+
+for (let i = 0; i < 2; i++) {
+    const a = +prompt('Один из последних просмотренных фильмов?', '23'),
+          b = +prompt('Оцените его по пятибальной шкале!', '5');
+    
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+        personalMovieDB.movies[a] = b;
+    } else {
+        i--;
+    }
+}
+
+// let i = 0;
+
+// while (i < 2) {
+//     const a = +prompt('Один из последних просмотренных фильмов?', '23'),
+//           b = +prompt('Оцените его по пятибальной шкале!', '5');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         i++
+//     } else {
+//         i--;
+//     }
+// }
+
+// let i = 0;
+
+// do {
+//     const a = +prompt('Один из последних просмотренных фильмов?', '23'),
+//           b = +prompt('Оцените его по пятибальной шкале!', '5');
+    
+//     if (a != null && b != null && a != '' && b != '' && a.length < 50) {
+//         personalMovieDB.movies[a] = b;
+//         i++
+//     } else {
+//         i--;
+//     }
+// } 
+// while (i < 2)
+
+
+if (personalMovieDB.count < 10 && personalMovieDB.count > 0) {
+    console.log('мало');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('норм');
+} else if (personalMovieDB.count >= 30) {
+    console.log('киноман');
+} else {
+    console.log('ошибка');
+}
+
+console.log(personalMovieDB);
+
